72b8ba010c304ca3e1ac85bc716d8721b4889cc5
Dhanashri Bhavsar <129056367+dhanashri-code@users.noreply.github.com>
2025-05-16 11:29:58 +0530

    Update authServices.jsx

--- client/src/services/authServices.jsx
+++ client/src/services/authServices.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const SERVER_URL = "http://localhost:5000"; 
+const SERVER_URL = "https://task-manager-app-yta0.onrender.com"; 
 
 const registerUser = (data) => {
     return axios.post(`${SERVER_URL}/register`, data);
